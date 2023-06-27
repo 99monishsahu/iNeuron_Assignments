c31c640ca357b821425c36068d719619642a73db
Monish Kumar Sahu <49946549+99monishsahu@users.noreply.github.com>
2023-06-27 19:34:08 +0530

    Update Array_Assignment1.cpp

--- Array_Assignment1.cpp
+++ Array_Assignment1.cpp
@@ -45,3 +45,67 @@ int main()
       if(flag) break;
     }
 }
+
+/*
+Question 2.
+Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+
+Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+
+- Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
+- Return k.
+
+Example :
+Input: nums = [3,2,2,3], val = 3
+Output: 2, nums = [2,2,_*,_*]
+
+Explanation: Your function should return k = 2, with the first two elements of nums being 2. It does not matter what you leave beyond the returned k (hence they are underscores)
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int val,size_of_array,i=0,count=0;
+    
+    cout<<"Enter size of array(nums): "<<endl;
+    cin>>size_of_array;
+    
+    int j = size_of_array-1;
+    
+    cout<<"Enter value: "<<endl;
+    cin>>val;
+    
+    int nums[size_of_array];
+    cout<<"Enter values in array(nums): "<<endl;
+    for(i=0; i<size_of_array; i++){
+      cin>>nums[i];
+    }
+    
+    //Main Code
+    for(i=0; i<size_of_array; i++){
+      if(nums[i]!=val) count++;
+    }
+    cout<<"The number of elements in nums which are not equal to val are: "<<count<<endl;
+    
+    i=0;
+    while(count){
+      if(nums[i]==val){
+        for(j=size_of_array-1; j>0; j--){
+          if(nums[i]!=nums[j]){
+            int temp = nums[i];
+            nums[i]=nums[j];
+            nums[j]=temp;
+            break;
+          }
+        }
+      }
+      i++;
+      count--;
+    }
+    
+    for(i=0; i<size_of_array; i++){
+      cout<<nums[i]<<",";
+    }
+}
