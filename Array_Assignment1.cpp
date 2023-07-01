ddefa5a3f4d8184c171bf83b3dfb7374d27420fc
Monish Kumar Sahu <49946549+99monishsahu@users.noreply.github.com>
2023-07-01 15:12:55 +0530

    Update Array_Assignment1.cpp

--- Array_Assignment1.cpp
+++ Array_Assignment1.cpp
@@ -46,22 +46,20 @@ int main()
     }
 }
 
-/*
-Question 2.
-Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of 
-elements in nums which are not equal to val.
+
+/*Q2. Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+
 Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
-    - Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important 
-      as well as the size of nums.
-    - Return k.
+
+- Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
+- Return k.
 
 Example :
 Input: nums = [3,2,2,3], val = 3
 Output: 2, nums = [2,2,_*,_*]
 
-Explanation: Your function should return k = 2, with the first two elements of nums being 2. It does not matter what you leave beyond the returned k 
-(hence they are underscores)
-*/
+Explanation: Your function should return k = 2, with the first two elements of nums being 2. It does not matter what you leave beyond the returned k (hence they are underscores)
+
 
 #include <iostream>
 using namespace std;
@@ -112,18 +110,67 @@ int main()
 }
 
 
-/*3. Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were 
-inserted in order.
+/*3. Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
 You must write an algorithm with O(log n) runtime complexity.
 
 Example 1:
 Input: nums = [1,3,5,6], target = 5
+
 Output: 2
 */
 
+#include <iostream>
+using namespace std;
 
-/*Q4. You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most 
-significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+int main() 
+{
+    int target,size_of_array,i=0;
+    
+    cout<<"Enter size of array(nums): "<<endl;
+    cin>>size_of_array;
+    
+    int end = size_of_array;
+    int mid = (i+end)/2;
+    
+    cout<<"Enter target value: "<<endl;
+    cin>>target;
+    
+    int nums[size_of_array];
+    cout<<"Enter values in array(nums): "<<endl;
+    for(i=0; i<size_of_array; i++){
+      cin>>nums[i];
+    }
+    
+    //Main Code
+    for(i=0; i<end; i=i+0){
+      mid = (i+end)/2;
+      if(nums[mid]==target){
+        cout<<mid;
+        break;
+      }
+      else{
+        if(i+1==end){
+          if(target<nums[i]){
+            cout<<"0";
+            break;
+          }
+          if(target>nums[end]){
+            cout<<end+1;
+            break;
+          }
+          cout<<i+1;
+          break;
+        }
+        if(nums[mid]<target) i = mid;
+        else end = mid;
+      }
+    }
+}
+
+/*Q4. You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. 
+The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+
 Increment the large integer by one and return the resulting array of digits.
 
 Example 1:
@@ -131,15 +178,54 @@ Input: digits = [1,2,3]
 Output: [1,2,4]
 
 Explanation: The array represents the integer 123.
+
 Incrementing by one gives 123 + 1 = 124.
 Thus, the result should be [1,2,4].
 */
+#include <iostream>
+using namespace std;
 
-/*Q5.You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 
-respectively.
+int main() 
+{
+    int size_of_array,i,carry=0;
+    
+    cout<<"Enter size of array: "<<endl;
+    cin>>size_of_array;
+    
+    int digits[size_of_array];
+    cout<<"Enter digits of Integer: "<<endl;
+    for(i=0; i<size_of_array; i++){
+      cin>>digits[i];
+    }
+    
+    //Main Code
+    for(i=size_of_array-1; i>=0; i--){
+      if(digits[i]<9){
+        digits[i]++;
+        carry=0;
+        break;
+      }
+      else{
+        digits[i]=0;
+        carry=1;
+      }
+    }
+    
+    if(carry){
+      cout<<"1,";
+    }
+    for(i=0; i<size_of_array; i++){
+      cout<<digits[i];
+      if(i<size_of_array-1) cout<<",";
+    }
+}
+
+
+/*Q5.You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
 Merge nums1 and nums2 into a single array sorted in non-decreasing order.
-The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, 
-where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+
+The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
 
 Example 1:
 Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
@@ -153,10 +239,12 @@ The result of the merge is [1,2,2,3,5,6] with the underlined elements coming fro
 
 Example 1:
 Input: nums = [1,2,3,1]
+
 Output: true
 */
 
 /*Q7. Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the nonzero elements.
+
 Note that you must do this in-place without making a copy of the array.
 
 Example 1:
@@ -164,9 +252,10 @@ Input: nums = [0,1,0,3,12]
 Output: [1,3,12,0,0]
 */
 
-/*Q8. You have a set of integers s, which originally contains all the numbers from 1 to n. Unfortunately, due to some error, one of the numbers in s got duplicated to 
-another number in the set, which results in repetition of one number and loss of another number.
+/*Q8. You have a set of integers s, which originally contains all the numbers from 1 to n. Unfortunately, due to some error, one of the numbers in s got duplicated to another number in the set, which results in repetition of one number and loss of another number.
+
 You are given an integer array nums representing the data status of this set after the error.
+
 Find the number that occurs twice and the number that is missing and return them in the form of an array.
 
 Example 1:
