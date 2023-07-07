4560c723d5f9bb38d9c4658015293f95ceef3517
Monish Kumar Sahu <49946549+99monishsahu@users.noreply.github.com>
2023-07-07 15:19:39 +0530

    Update Array_Assignment1.cpp

--- Array_Assignment1.cpp
+++ Array_Assignment1.cpp
@@ -1,9 +1,7 @@
 /*
 QUESTION 1.
 Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
 You can return the answer in any order.
 
 Example:
@@ -47,11 +45,11 @@ int main()
 }
 
 
-/*Q2. Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
-
+/*Q2. Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the 
+number of elements in nums which are not equal to val.
 Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
-
-- Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
+- Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not 
+  important as well as the size of nums.
 - Return k.
 
 Example :
@@ -110,8 +108,8 @@ int main()
 }
 
 
-/*3. Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
-
+/*3. Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if 
+it were inserted in order.
 You must write an algorithm with O(log n) runtime complexity.
 
 Example 1:
@@ -170,7 +168,6 @@ int main()
 
 /*Q4. You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. 
 The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
-
 Increment the large integer by one and return the resulting array of digits.
 
 Example 1:
@@ -178,7 +175,6 @@ Input: digits = [1,2,3]
 Output: [1,2,4]
 
 Explanation: The array represents the integer 123.
-
 Incrementing by one gives 123 + 1 = 124.
 Thus, the result should be [1,2,4].
 */
@@ -221,11 +217,11 @@ int main()
 }
 
 
-/*Q5.You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
-
+/*Q5.You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 
+and nums2 respectively.
 Merge nums1 and nums2 into a single array sorted in non-decreasing order.
-
-The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of 
+m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
 
 Example 1:
 Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
@@ -326,20 +322,50 @@ int main()
     
     for(i=0; i<m+n; i++){
       cout<<nums1[i];
+      if(i<size_of_array-1) cout<<",";
     }
 }
 
 
 /*Q6. Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
-
 Example 1:
 Input: nums = [1,2,3,1]
-
 Output: true
 */
 
-/*Q7. Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the nonzero elements.
+#include <bits/stdc++.h>
+using namespace std;
 
+
+int main() 
+{
+    int m,i,flag=0;
+    
+    cout<<"Enter size of array: "<<endl;
+    cin>>m;
+    
+    int nums[m], arr[1000000]={0};
+    cout<<"Enter Integer values in nums : "<<endl;
+    for(i=0; i<m; i++){
+      cin>>nums[i];
+    }
+    
+    //Main Code
+    for(i=0; i<m; i++){
+      arr[nums[i]]++;
+    }
+    
+    for(i=0; i<m; i++){
+      if(arr[nums[i]]>1){
+        cout<<"True"<<endl;
+        flag=1;
+        break;
+      }
+    }
+    if(flag==0) cout<<"False"<<endl;
+}
+
+/*Q7. Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the nonzero elements.
 Note that you must do this in-place without making a copy of the array.
 
 Example 1:
@@ -347,10 +373,46 @@ Input: nums = [0,1,0,3,12]
 Output: [1,3,12,0,0]
 */
 
-/*Q8. You have a set of integers s, which originally contains all the numbers from 1 to n. Unfortunately, due to some error, one of the numbers in s got duplicated to another number in the set, which results in repetition of one number and loss of another number.
+#include <bits/stdc++.h>
+using namespace std;
 
+
+int main() 
+{
+    int m,i,j,flag=0;
+    
+    cout<<"Enter size of array: "<<endl;
+    cin>>m;
+    
+    int nums[m];
+    cout<<"Enter Integer values in nums : "<<endl;
+    for(i=0; i<m; i++){
+      cin>>nums[i];
+    }
+    
+    //Main Code
+    
+    for(i=0; i<m; i++){
+      if(nums[i]!=0){
+        for(j=0; j<m; j++){
+          if(nums[j]==0){
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+          }
+        }
+      }
+    }
+    
+    for(i=0; i<m; i++){
+      cout<<nums[i];
+      if(i<m-1) cout<<",";
+    }
+}
+
+/*Q8. You have a set of integers s, which originally contains all the numbers from 1 to n. Unfortunately, due to some error, one of the numbers in s got 
+duplicated to another number in the set, which results in repetition of one number and loss of another number.
 You are given an integer array nums representing the data status of this set after the error.
-
 Find the number that occurs twice and the number that is missing and return them in the form of an array.
 
 Example 1:
