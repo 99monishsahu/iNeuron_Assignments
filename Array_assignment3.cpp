085b137e061630ad96c9912bee0c93a9cdf3a264
Monish Kumar Sahu <49946549+99monishsahu@users.noreply.github.com>
2023-07-14 15:23:08 +0530

    Update Array_assignment3.cpp

--- Array_assignment3.cpp
+++ Array_assignment3.cpp
@@ -91,7 +91,36 @@ You are given a large integer represented as an integer array digits, where each
 significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
 Increment the large integer by one and return the resulting array of digits.
 */
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int i=digits.size()-1;
+        if(digits[i]!=9) {
+            digits[i]++;
+            return digits;
+        }
+        else{
+            for(i=digits.size()-1; i>=0; i--){
+                if(digits[i]!=9){
+                    digits[i]++;
+                    return digits;
+                }
+                else{
+                    if(i==0){
+                        digits[i]=1;
+                        digits.push_back(0);
+                        return digits;
+                    }
+                    else{
+                        digits[i]=0;
+                    }
+                }
+            }
+        }
 
+        return digits;
+    }
+};
 
 /*
 Question 6
